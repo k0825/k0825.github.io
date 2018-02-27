c1e4bfa97529868901b3392d9f755b8992e7bccd
k0825 <kazukkiyo@icloud.com>
2018-02-27 20:32:40 +0900

    init test-1

---
+++ Top/toppage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE=html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>kazuのページ</title>
+    <link ref="stylesheet" href="/stylesheet/style.css">
+</head>
+<body>
+    <div class="header">
+        <div class="logo">あ</div>
+        <ul>
+            <li><a href="#">トップ</a></li>
+            <li><a href="#">ガレキ</a></li>
+            <li><a href="#">自己紹介</a></li>
+            <li><a href="#">ブログ</a></li>
+        </ul>
+    </div>
+</body>
+</html>
